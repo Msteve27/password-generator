50e585ceb2b3a092ca86d25d1eabd46ee148d0e6
Msteve27 <msteve27@vols.utk.edu>
2021-10-25 10:28:04 -0500

    upper+lowercase functions

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,12 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -15,3 +15,39 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// prompt: enter preferred length of password (select from 8 - 128 characters)
+    // --> function needs to iterate over 26 alphabetic characters 
+    // --> random [lowercase alphabet array]
+    // --> random [uppercase alphabet array]
+    // --> random [number between 0-9] 
+    // --> random [special character array] 
+
+// prompt: do you want an uppercase letter? (yes or no)
+  // -- > if, else statements 
+
+// prompt: do you want your password to contain a number? (yes or no)
+  // --> if, else statements 
+
+// prompt: do you want your password to contain a special character? (yes or no)
+  // --> if, else statements
+
+  const lowercase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+  const uppercase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+  const special = [!,",#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~]
+  const randomNumber = Math.floor(Math.random(0-10))
+  
+  function callLowercase () {
+      return String.fromCharCode(Math.floor(Math.random()*26)+97)
+    }
+  
+  function callUppercase () {
+      return String.fromCharCode(Math.floor(Math.random()*26)+65)
+    }
+  
+    console.log(callUppercase());
+
+
