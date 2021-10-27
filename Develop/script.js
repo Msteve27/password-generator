9032334e8ee976221103d5890db64f2f184d5050
Msteve27 <msteve27@vols.utk.edu>
2021-10-27 11:15:42 -0500

    added if loops

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,87 @@
 // Assignment code here
-let promptPWlength = window.prompt("How long do you want your password to be? Must be between 8 and 128 characters.");
-let promptlowercase = window.prompt("Do you want to use lowercase letters? Keep in mind you will need at least one character type selected.");
-let promptuppercase = window.prompt("Do you want to use uppercase letters?");
+let PWlength = window.prompt("How long do you want your password to be? Must be between 8 and 128 characters.");
+let promptlowercase = window.prompt("Do you want to use lowercase letters?");
+let promptuppercase = window.prompt("Do you want to use uppercase letters? Keep in mind you will need at least one character type selected.");
 let promptnumber = window.prompt("Do you want to use a number?");
 let promptsymbol = window.prompt("Do you want to use a symbol?");
+// let password = document.getElementById("password");
 
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
-
-  // if a person picks a length not between 8 - 128, break
-  if (promptPWlength < 8 && promptPWlength > 128)
-      // tell the user password must be between 8 and 128 characters in length
-      window.prompt("Password must be between 8 and 128 characters in length.");
-      return; 
-  }
-  // validate the inputs (at least one special character, and length)
-  // generate PW
-
-  // function getLower () {
-  //   (String.fromCharCode(Math.floor(Math.random()*26)+97))
-  // }
-  
-  // function getUpper () {
-  //   (String.fromCharCode(Math.floor(Math.random()*26)+65))
-  // }
-    
-  // function getNumber () {
-  //   (String.fromCharCode(Math.floor(Math.random()*10)+48))
-  // }
-  
-  // function getSymbol () {
-  //   const symbols = '!#$%&()*+,-./:;<=>?@[]^_`{|}~';
-  //   (symbols [Math.floor(Math.random() * symbols.length)]);
-  // }
-
-
-
-  // generated PW displayed
-  //return "generated password will go here";
-
 
 // Write password to the #password input
-function writePassword() {
+// function writePassword() {
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+let generatePassword = function () {
+  if (PWlength < 8 && PWlength > 128) {
+    console.log(true)
+    alert("Password must be between 8 and 128 characters in length.")
+    return false; 
+  }
+  for (let i = 0; i <= PWlength; i++) {
+    if (promptlowercase === "yes" || promptlowercase === "YES") {
+      getLower ();
+    } else { (promptlowercase === "no" || promptlowercase === "NO")
+      alert("Enter 'yes' or 'no.'")
+      return false; 
+    }
+    if (promptuppercase === "yes" || promptuppercase === "YES") {
+      getUpper ();
+    } else { (promptuppercase === "no" || promptuppercase === "NO")
+      alert("Enter 'yes' or 'no.'")
+      return false; 
+    }
+    if (promptnumber === "yes" || promptnumber === "YES") {
+      getNumber ();
+    } else { (promptnumber === "no" || promptnumber === "NO")
+      alert("Enter 'yes' or 'no.'")
+      return false; 
+    }
 
+    if (promptsymbol === "yes" || promptsymbol === "YES") {
+      getSymbol ();
+    } else { (promptsymbol === "no" || promptsymbol === "NO")
+      alert("Enter 'yes' or 'no.'")
+      return false; 
+    }
+
+
+    // let getlower = (String.fromCharCode(Math.floor(Math.random()*26)+97));
+    // password += // add chunks of password together;
+  }
+
+};
+
+// validate the inputs (at least one special character, and length)
+// generate PW
+
+
+
+let getLower = function () {
+  (String.fromCharCode(Math.floor(Math.random()*26)+97))
 }
 
+let getUpper = function () {
+  (String.fromCharCode(Math.floor(Math.random()*26)+65))
+}
+  
+let getNumber = function () {
+  (String.fromCharCode(Math.floor(Math.random()*10)+48))
+}
+
+let getSymbol = function () {
+  const symbols = '!#$%&()*+,-./:;<=>?@[]^_`{|}~';
+  (symbols [Math.floor(Math.random() * symbols.length)]);
+}
+
+
+passwordText.value = password;
+
+
+document.getElementById("password").value = password;
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// }
