c770899a04c8584b5e793d252b30ccf0fd054752
Msteve27 <msteve27@vols.utk.edu>
2021-10-29 12:17:40 -0500

    final commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,87 +1,83 @@
 // Assignment code here
-let PWlength = window.prompt("How long do you want your password to be? Must be between 8 and 128 characters.");
-let promptlowercase = window.prompt("Do you want to use lowercase letters?");
-let promptuppercase = window.prompt("Do you want to use uppercase letters? Keep in mind you will need at least one character type selected.");
-let promptnumber = window.prompt("Do you want to use a number?");
-let promptsymbol = window.prompt("Do you want to use a symbol?");
-// let password = document.getElementById("password");
-
+// Arrays:
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const symbols = ["'", "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"]
+  
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
 // function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+// const password = generatePassword();
 
-let generatePassword = function () {
-  if (PWlength < 8 && PWlength > 128) {
-    console.log(true)
+
+
+
+function generatePassword() {
+
+  let PWLength = parseInt(prompt("How long do you want your password to be? Must be between 8 and 128 characters."))
+
+  if (isNaN(PWLength)) {
+    alert('Password length must be a number.')
+    return
+  }
+
+  if (PWLength < 8 || PWLength > 128) {
     alert("Password must be between 8 and 128 characters in length.")
-    return false; 
-  }
-  for (let i = 0; i <= PWlength; i++) {
-    if (promptlowercase === "yes" || promptlowercase === "YES") {
-      getLower ();
-    } else { (promptlowercase === "no" || promptlowercase === "NO")
-      alert("Enter 'yes' or 'no.'")
-      return false; 
-    }
-    if (promptuppercase === "yes" || promptuppercase === "YES") {
-      getUpper ();
-    } else { (promptuppercase === "no" || promptuppercase === "NO")
-      alert("Enter 'yes' or 'no.'")
-      return false; 
-    }
-    if (promptnumber === "yes" || promptnumber === "YES") {
-      getNumber ();
-    } else { (promptnumber === "no" || promptnumber === "NO")
-      alert("Enter 'yes' or 'no.'")
-      return false; 
-    }
-
-    if (promptsymbol === "yes" || promptsymbol === "YES") {
-      getSymbol ();
-    } else { (promptsymbol === "no" || promptsymbol === "NO")
-      alert("Enter 'yes' or 'no.'")
-      return false; 
-    }
-
-
-    // let getlower = (String.fromCharCode(Math.floor(Math.random()*26)+97));
-    // password += // add chunks of password together;
+    return
   }
 
-};
+  let promptLowercase = window.confirm("Do you want to use lowercase letters?");
+  let promptUppercase = window.confirm("Do you want to use uppercase letters? Keep in mind you will need at least one character type selected.");
+  let promptNumber = window.confirm("Do you want to use a number?");
+  let promptSymbol = window.confirm("Do you want to use a symbol?");
 
-// validate the inputs (at least one special character, and length)
-// generate PW
+  let Characters = []
 
+  if (promptLowercase) {
+    Characters = Characters.concat(lowercase)
+  }
 
-
-let getLower = function () {
-  (String.fromCharCode(Math.floor(Math.random()*26)+97))
-}
-
-let getUpper = function () {
-  (String.fromCharCode(Math.floor(Math.random()*26)+65))
-}
+  if (promptUppercase) {
+    Characters = Characters.concat(uppercase)
+  }
   
-let getNumber = function () {
-  (String.fromCharCode(Math.floor(Math.random()*10)+48))
+  if (promptNumber) {
+    Characters = Characters.concat(numbers)
+  }
+
+  if (promptSymbol) {
+    Characters = Characters.concat(symbols)
+  }
+
+  let PWString = '';
+  
+  for (let i = 0; i < PWLength; i++) {
+    let randomNumbers = Math.floor(Math.random() * Characters.length)
+
+    let randomCharacters = Characters[randomNumbers]
+    
+    PWString = PWString + randomCharacters
+  }
+
+  return PWString
 }
 
-let getSymbol = function () {
-  const symbols = '!#$%&()*+,-./:;<=>?@[]^_`{|}~';
-  (symbols [Math.floor(Math.random() * symbols.length)]);
+function writePassword() {
+  let password = generatePassword();
+  if (password === undefined) {
+    password = 'Click "Generate Password" to try again.'
+  }
+  let passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
+  passwordText.innerHTML = password;
+
 }
 
-
-passwordText.value = password;
-
-
-document.getElementById("password").value = password;
+// document.getElementById("password").value = password;
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// }
